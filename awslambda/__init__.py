d8fcb58a629faa15c6c704b8f0d431f5cdd85a4c
Kyle Finley <kyle@finley.sh>
2021-09-22 13:20:35 -0400

    add `__version__` to project (#71)
    * add `__version__` to project
    
    * use package version in docs
    
    * exclude __version__ from test coverage

--- awslambda/__init__.py
+++ awslambda/__init__.py
@@ -1,4 +1,24 @@
 """AWS Lambda hooks."""
+import sys
+
 from ._python import PythonFunction
 
-__all__ = ["PythonFunction"]
+if sys.version_info < (3, 8):  # cov: ignore
+    # importlib.metadata is standard lib for python>=3.8, use backport
+    from importlib_metadata import (  # type: ignore # pylint: disable=E
+        PackageNotFoundError,
+        version,
+    )
+else:
+    from importlib.metadata import (  # type: ignore # pylint: disable=E
+        PackageNotFoundError,
+        version,
+    )
+
+__all__ = ["__version__", "PythonFunction"]
+
+try:  # cov: ignore
+    __version__ = version(__name__)
+except PackageNotFoundError:  # cov: ignore
+    # package is not installed
+    __version__ = "0.0.0"
