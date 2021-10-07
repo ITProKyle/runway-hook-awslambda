f6df0dd74e79b147ce48295cc0623ab28322467d
Kyle Finley <kyle@finley.sh>
2021-10-07 10:32:32 -0400

    add AWS SAM build image prefix for docker images

--- awslambda/constants.py
+++ awslambda/constants.py
@@ -1,4 +1,7 @@
 """Constant values."""
 from runway.constants import DOT_RUNWAY_DIR
 
+AWS_SAM_BUILD_IMAGE_PREFIX = (  #: Prefix for build image registries.
+    "public.ecr.aws/sam/build-"
+)
 BASE_WORK_DIR = DOT_RUNWAY_DIR / "awslambda"
